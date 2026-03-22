1c9769c2f602551d3c7bab3658811e0e24336400
HellOfSpades <burbonx@gmail.com>
2026-03-23 00:32:28 +0100

    Try to implement the simplest dag.

---
+++ dags/TestDag.py
@@ -0,0 +1,16 @@
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.sdk import dag, task
+from pendulum import datetime
+
+
+@dag(start_date=datetime(2020, 12, 31),
+     schedule="@daily",
+     tags=["activity", "custom dag"],
+     catchup=False
+     )
+def test_dag():
+    print("good")
+    pass
+
+
+test_dag()
