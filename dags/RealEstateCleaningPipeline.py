fcb30f83e3ca6437843867be0c7619d45cfa7fee
HellOfSpades <burbonx@gmail.com>
2026-03-23 20:36:54 +0100

    Add call to task1

--- dags/RealEstateCleaningPipeline.py
+++ dags/RealEstateCleaningPipeline.py
@@ -1,63 +1,36 @@
 from pathlib import Path
 
-from airflow import DAG
-from datetime import datetime
 import pandas as pd
-import os
-
 from airflow.providers.standard.operators.python import PythonOperator
-
-BASE_DIR = Path(__file__).resolve().parents[1]
-
-INPUT_CSV = BASE_DIR / "data/raw/Real_Estate_Sales_Raw.csv"
-OUTPUT_CSV = BASE_DIR / "data/cleaned/Address.csv"
+from airflow.sdk import dag, task
+from pendulum import datetime
 
 
-def transform_property_type_csv(input_csv: str, output_csv: str) -> None:
-    df = pd.read_csv(input_csv)
-
-    required_columns = ["Property Type", "Residential Type"]
-    missing = [col for col in required_columns if col not in df.columns]
-    if missing:
-        raise ValueError(f"Missing required columns: {missing}")
-
-    mask = (
-        df["Property Type"].eq("Residential")
-        & df["Residential Type"].notna()
-    )
-
-    df.loc[mask, "Property Type"] = df.loc[mask, "Residential Type"]
-
-    df = df.drop(columns=["Residential Type"])
-
-    os.makedirs(os.path.dirname(output_csv), exist_ok=True)
-    df.to_csv(output_csv, index=False)
+@dag(start_date=datetime(2026, 1, 1),
+     schedule="@daily",
+     tags=["activity", "custom dag"],
+     catchup=False
+     )
+def clean_real_estate_pipeline():
+    @task
+    def write_to_csv(df, dir):
+        df.to_csv(dir, index=False)
+    @task
+    def get_root_dir():
+        return Path(__file__).resolve().parents[1]
+    @task
+    def load_csv(path):
+        return pd.read_csv(path, dtype=str)
 
 
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "retries": 1,
-}
+    BASE_DIR = get_root_dir()
+
+    main_path = BASE_DIR / "data/raw/Real_Estate_Sales_Raw.csv"
+    output_path = BASE_DIR / "data/cleaned/Real_Estate_Sales.csv"
+
+    main_df = load_csv(main_path)
+
+    write_to_csv(main_df, output_path)
 
 
-with DAG(
-    dag_id="transform_property_type_csv_dag",
-    default_args=default_args,
-    description="Replace Residential property type with Residential Type values and drop Residential Type column",
-    start_date=datetime(2025, 1, 1),
-    schedule_interval=None,
-    catchup=False,
-    tags=["csv", "pandas", "cleanup"],
-) as dag:
-
-    transform_csv_task = PythonOperator(
-        task_id="transform_property_type_csv",
-        python_callable=transform_property_type_csv,
-        op_kwargs={
-            "input_csv": INPUT_CSV,
-            "output_csv": OUTPUT_CSV,
-        },
-    )
-
-    transform_csv_task
+clean_real_estate_pipeline()
--- dags/TestDag.py
+++ dags/TestDag.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from airflow.providers.standard.operators.python import PythonOperator
 from airflow.sdk import dag, task
 from pendulum import datetime
@@ -12,7 +14,15 @@ def test_dag():
     @task
     def task1():
         return "Task Return Value"
-
     task1()
+
+    BASE_DIR = Path(__file__).resolve().parents[1]
+
+    main_path = BASE_DIR / "data/raw/Real_Estate_Sales_Raw.csv"
+    ref_path = BASE_DIR / "data/cleaned/Real_Estate_Sales.csv"
+
+
+
+
 
 test_dag()
